0ecc3855b66ddd9bc2bbd3e9fef8481dc3e440a1
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-07-21 09:18:30 +0900

    unity-command v1.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import * as core from '@actions/core'
-import { Unity } from '@akiojin/unity-command'
+import { UnityUtils } from '@akiojin/unity-command'
 
 async function Run()
 {
 	try {
-		const version = await Unity.GetVersion(core.getInput('project-directory') || process.cwd())
+		const version = await UnityUtils.GetVersion(core.getInput('project-directory') || process.cwd())
 
 		core.setOutput('version', version)
 		core.info(`Unity version: ${version}`)
